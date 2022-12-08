236014628fda58b42e8192f550f95912bbe8db78
lucadard <lucadardenne@hotmail.com>
2022-12-08 15:21:25 -0300

    update const from env var

--- src/databases/config.ts
+++ src/databases/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 if (process.env.NODE_ENV !== 'production') dotenv.config()
 
-const MONGO_URL = process.env.MONGO_URL!
+const MONGO_URL = process.env.MONGO_URL
 
 export default {
   fileSys: {
